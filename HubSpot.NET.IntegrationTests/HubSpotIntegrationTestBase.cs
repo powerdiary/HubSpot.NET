a5fa37e058748bf0e79b329bfcb669ce74684237
Oleg S <lom200@gmail.com>
2024-05-07 12:55:35 +1000

    Add integration tests to the project
    This commit introduces integration tests to the project, creating a new IntegrationTests project and adding it to the solution. A new "appsettings.json" file is added for the project to store the app's necessary configurations. Changes are also made to gitignore to ignore development and production "appsettings.json" files.

---
+++ HubSpot.NET.IntegrationTests/HubSpotIntegrationTestBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HubSpot.NET.IntegrationTests;
+
+public class HubSpotIntegrationTestBase
+{
+        protected string? ApiKey;
+
+        protected HubSpotIntegrationTestBase()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true)
+                .Build();
+
+            ApiKey = configuration["HubSpot:PrivateAppKey"];
+        }
+}
