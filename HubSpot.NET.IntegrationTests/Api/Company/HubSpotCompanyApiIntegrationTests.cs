a0699f151d2bd4a5b415f60a2fc6d5ec4368f538
Oleg S <lom200@gmail.com>
2024-05-07 12:58:17 +1000

    Add HubSpot Company API integration tests
    The integration tests for the HubSpot Company API were added using the FluentAssertions library for assertions. Additionally, the FluentAssertions library was added as a package reference in the project. The tests include creating and deleting a company.

---
+++ HubSpot.NET.IntegrationTests/Api/Company/HubSpotCompanyApiIntegrationTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using HubSpot.NET.Api.Company;
+using HubSpot.NET.Api.Company.Dto;
+using HubSpot.NET.Core;
+
+namespace HubSpot.NET.IntegrationTests.Api.Company;
+
+public class HubSpotCompanyApiIntegrationTests: HubSpotIntegrationTestBase
+{
+    private readonly HubSpotCompanyApi _api;
+
+    public HubSpotCompanyApiIntegrationTests()
+    {
+        var client = new HubSpotBaseClient(ApiKey);
+        _api = new HubSpotCompanyApi(client);
+    }
+
+    [Fact]
+    public void CreateAndDeleteCompany()
+    {
+        CompanyHubSpotModel? createdCompany = null;
+
+        try
+        {
+            var newCompany = new CompanyHubSpotModel
+            {
+                Name = "Test Company",
+                Country = "Australia",
+                Website = "https://www.test.com"
+            };
+
+            createdCompany = _api.Create(newCompany);
+
+            using (new AssertionScope())
+            {
+                createdCompany.Should().NotBeNull();
+                createdCompany.Id.Should().HaveValue();
+            }
+        }
+        finally
+        {
+            if (createdCompany != null)
+            {
+                _api.Delete(createdCompany.Id.Value);
+            }
+        }
+    }
+}
