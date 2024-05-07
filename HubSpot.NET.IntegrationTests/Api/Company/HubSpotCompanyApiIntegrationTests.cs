446ca4ee8c81bf5d88dcda05cffec2f948ee2733
Oleg S <lom200@gmail.com>
2024-05-07 13:39:40 +1000

    Rename test method in Company API integration tests
    Renamed the test method 'CreateAndDeleteCompany' to 'CreateAndDeleteCompanyTest' in HubSpotCompanyApiIntegrationTests class to make it more descriptive and align with naming conventions.

--- HubSpot.NET.IntegrationTests/Api/Company/HubSpotCompanyApiIntegrationTests.cs
+++ HubSpot.NET.IntegrationTests/Api/Company/HubSpotCompanyApiIntegrationTests.cs
@@ -46,4 +46,36 @@ public class HubSpotCompanyApiIntegrationTests: HubSpotIntegrationTestBase
             }
         }
     }
+
+    [Fact]
+    public void GetCompanyById()
+    {
+        var createdCompany = _api.Create(new CompanyHubSpotModel
+        {
+            Name = "Test Company",
+            Country = "Australia",
+            Website = "https://www.test.com"
+        });
+
+        try
+        {
+            var companyById = _api.GetById<CompanyHubSpotModel>(createdCompany.Id.Value);
+
+            using (new AssertionScope())
+            {
+                companyById.Should().NotBeNull();
+                companyById.Id.Should().Be(createdCompany.Id);
+                companyById.Name.Should().Be(createdCompany.Name);
+                companyById.Country.Should().Be(createdCompany.Country);
+                companyById.Website.Should().Be(createdCompany.Website);
+            }
+        }
+        finally
+        {
+            if (createdCompany != null)
+            {
+                _api.Delete(createdCompany.Id.Value);
+            }
+        }
+    }
 }
