508d88147953da03d5de33e860899db71b4870a6
Oleg S <lom200@gmail.com>
2024-05-10 16:44:12 +1000

    Refactor PropertiesListHubSpotModel class in HubSpot API
    Removed ICollection implementation and replaced `Properties` List with `Results` to reflect the structure of the API response data. The new data model allows for easier data handling and better compatibility with HubSpot data structure.

--- HubSpot.NET/Api/Properties/Dto/PropertiesListHubSpotModel.cs
+++ HubSpot.NET/Api/Properties/Dto/PropertiesListHubSpotModel.cs
@@ -1,74 +1,31 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using HubSpot.NET.Core.Interfaces;
 
-namespace HubSpot.NET.Api.Properties.Dto
+namespace HubSpot.NET.Api.Properties.Dto;
+
+[DataContract]
+public class PropertiesListHubSpotModel<T> : IHubSpotModel where T : IHubSpotModel
 {
+    [DataMember(Name = "results")] public List<T> Results { get; set; }
 
-    /// <summary>
-    /// Models a set of properties in HubSpot (contacts, companies etc.)
-    /// </summary>
-    [DataContract]
-    public class PropertiesListHubSpotModel<T> : IHubSpotModel, ICollection<T> where T : IHubSpotModel
+    public string RouteBasePath
     {
-        private List<T> Properties { get; } = new List<T>();
-
-        public string RouteBasePath
+        get
         {
-            get
-            {
-                var entity = (T)Activator.CreateInstance(typeof(T));
-                return "crm/v3/properties" + entity.RouteBasePath;
-            }
+            var entity = (T)Activator.CreateInstance(typeof(T));
+            return "crm/v3/properties" + entity.RouteBasePath;
         }
+    }
 
-        public bool IsNameValue => false;
-        public virtual void ToHubSpotDataEntity(ref dynamic converted)
-        {
-        }
+    public bool IsNameValue => false;
 
-        public virtual void FromHubSpotDataEntity(dynamic hubspotData)
-        {
-        }
+    public virtual void ToHubSpotDataEntity(ref dynamic dataEntity)
+    {
+    }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return Properties.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public void Add(T item)
-        {
-            Properties.Add(item);
-        }
-
-        public void Clear()
-        {
-            Properties.Clear();
-        }
-
-        public bool Contains(T item)
-        {
-            return Properties.Contains(item);
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            Properties.CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(T item)
-        {
-            return Properties.Remove(item);
-        }
-
-        public int Count => Properties.Count;
-        public bool IsReadOnly => false;
+    public virtual void FromHubSpotDataEntity(dynamic hubspotData)
+    {
     }
 }
