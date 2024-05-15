5866b8ae76e96d5778ed9079cef62be2acc736de
Oleg S <lom200@gmail.com>
2024-05-15 14:42:52 +1000

    Refactor HubSpotAsyncIntegrationTestBase and add new tests
    This commit includes code refactoring in the HubSpotAsyncIntegrationTestBase class to improve code readability and maintainability. The update primarily covers the reformatting of async methods. Additionally, two sets of new tests have been added for ContactList and CompanyProperties in HubSpotContactListApiAsyncIntegrationTests and HubSpotCompanyPropertiesApiAsyncIntegrationTests respectively.

---
+++ HubSpot.NET.IntegrationTests/Api/ContactList/HubSpotContactListApiAsyncIntegrationTests.cs
@@ -0,0 +1,147 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using HubSpot.NET.Api.Contact.Dto;
+using HubSpot.NET.Api.ContactList.Dto;
+
+namespace HubSpot.NET.IntegrationTests.Api.ContactList;
+
+public class HubSpotContactListApiAsyncIntegrationTests : HubSpotAsyncIntegrationTestBase
+{
+    [Fact]
+    public async Task CreateStaticContactList()
+    {
+        var expectedContactListName = "StaticTestList";
+        var createdContactList = await RecreateTestContactListAsync(expectedContactListName);
+
+        using (new AssertionScope())
+        {
+            createdContactList.Should().NotBeNull();
+            createdContactList.Name.Should().Be(expectedContactListName);
+            createdContactList.Dynamic.Should().Be(false);
+        }
+    }
+
+    [Fact]
+    public async Task GetContactListById()
+    {
+        var createdContactList = await RecreateTestContactListAsync("StaticTestList");
+        var contactListById = await ContactListApi.GetContactListByIdAsync(createdContactList.ListId);
+
+        using (new AssertionScope())
+        {
+            contactListById.Should().NotBeNull();
+            contactListById.ListId.Should().Be(createdContactList.ListId);
+            contactListById.Name.Should().Be(createdContactList.Name);
+        }
+    }
+
+    [Fact]
+    public async Task ContactListById_WhenDoesNotExist_ShouldBeNull()
+    {
+        var nonExistingId = 0;
+
+        var contactListById = await ContactListApi.GetContactListByIdAsync(nonExistingId);
+
+        using (new AssertionScope())
+        {
+            contactListById.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public async Task AddContactsToList()
+    {
+        var (contact1, contact2, contactList) = await CreateContactsAndList();
+
+        var additionResult =
+            await ContactListApi.AddContactsToListAsync(contactList.ListId, new[] { contact1.Id.Value, contact2.Id.Value });
+
+        using (new AssertionScope())
+        {
+            additionResult.UpdatedContactIds.Should().Contain(new[] { contact1.Id.Value, contact2.Id.Value });
+        }
+    }
+
+    [Fact]
+    public async Task RemoveContactsFromList()
+    {
+        var (contact1, contact2, contactList) = await CreateContactsAndList();
+
+        await ContactListApi.AddContactsToListAsync(contactList.ListId, new[] { contact1.Id.Value, contact2.Id.Value });
+
+        var removalResult =
+            await ContactListApi.RemoveContactsFromListAsync(contactList.ListId, new[] { contact1.Id.Value, contact2.Id.Value });
+
+        using (new AssertionScope())
+        {
+            removalResult.UpdatedContactIds.Should().Contain(new[] { contact1.Id.Value, contact2.Id.Value });
+        }
+    }
+
+    [Fact]
+    public async Task DeleteContactList()
+    {
+        var createdContactList = await RecreateTestContactListAsync("StaticTestList");
+        await ContactListApi.DeleteContactListAsync(createdContactList.ListId);
+
+        var deletedContactList = await ContactListApi.GetContactListByIdAsync(createdContactList.ListId);
+
+        using (new AssertionScope())
+        {
+            deletedContactList.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public async Task GetContactLists()
+    {
+        var createdContactList1 = await RecreateTestContactListAsync("TestList1");
+        var createdContactList2 = await RecreateTestContactListAsync("TestList2");
+
+        // to make data searchable
+        await Task.Delay(10000);
+
+        var contactLists = (await ContactListApi.GetContactListsAsync()).Lists;
+
+        using (new AssertionScope())
+        {
+            contactLists.Should().NotBeNull();
+            contactLists.Should().Contain(c => c.ListId == createdContactList1.ListId);
+            contactLists.Should().Contain(c => c.ListId == createdContactList2.ListId);
+        }
+    }
+
+    // Note: The HubSpotContactListApi currently only allows for the creation of static lists,
+    // which is why these tests are designed around them. If future updates to the library
+    // add the ability to create dynamic lists, these tests should be revised to account for that.
+    [Fact]
+    public async Task GetStaticContactLists()
+    {
+        var createdContactList1 = await RecreateTestContactListAsync("StaticTestList1");
+        var createdContactList2 = await RecreateTestContactListAsync("StaticTestList2");
+
+        // to make data searchable
+        await Task.Delay(10000);
+
+        var contactLists = (await ContactListApi.GetStaticContactListsAsync()).Lists;
+
+        using (new AssertionScope())
+        {
+            contactLists.Should().NotBeNull();
+            contactLists.Should().Contain(c => c.ListId == createdContactList1.ListId);
+            contactLists.Should().Contain(c => c.ListId == createdContactList2.ListId);
+        }
+    }
+
+    private async Task<(ContactHubSpotModel contact1, ContactHubSpotModel contact2, ContactListModel contactList)>
+        CreateContactsAndList()
+    {
+        var createdContact1 = await RecreateTestContactAsync("createdcontact1@test.com", "FirstName1", "LastName1", "TestCompany1",
+            "1234567891");
+        var createdContact2 = await RecreateTestContactAsync("createdcontact2@test.com", "FirstName2", "LastName2", "TestCompany2",
+            "1234567892");
+        var createdContactList = await RecreateTestContactListAsync("StaticTestList");
+
+        return (createdContact1, createdContact2, createdContactList);
+    }
+}
---
+++ HubSpot.NET.IntegrationTests/Api/Properties/HubSpotCompanyPropertiesApiAsyncIntegrationTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using HubSpot.NET.Core;
+
+namespace HubSpot.NET.IntegrationTests.Api.Properties;
+
+public class HubSpotCompanyPropertiesApiAsyncIntegrationTests : HubSpotAsyncIntegrationTestBase
+{
+    [Fact(Skip = "This test requires CreatePropertyGroup implementation")]
+    public async Task Create()
+    {
+        const string expectedName = "TestProperty";
+        const string expectedType = "string";
+        const string expectedFieldType = "text";
+        const string expectedGroup = "TestGroup";
+        const string expectedLabel = "TestLabel";
+
+        var createdCompanyProperty =
+            await RecreateTestCompanyPropertyAsync(name: expectedName, type: expectedType,
+                fieldType: expectedFieldType,
+                groupName: expectedGroup, label: expectedLabel);
+
+        using (new AssertionScope())
+        {
+            createdCompanyProperty.Should().NotBeNull();
+            createdCompanyProperty.Name.Should().Be(expectedName);
+            createdCompanyProperty.Type.Should().Be(expectedType);
+            createdCompanyProperty.FieldType.Should().Be(expectedFieldType);
+        }
+    }
+
+    [Fact(Skip = "This test requires CreatePropertyGroup implementation")]
+    public async Task GetAll()
+    {
+        var createdCompanyProperty1 =
+            await RecreateTestCompanyPropertyAsync(name: "TestProperty1", type: "string", fieldType: "text");
+        var createdCompanyProperty2 =
+            await RecreateTestCompanyPropertyAsync(name: "TestProperty2", type: "string", fieldType: "text");
+
+        var allProperties = CompanyPropertiesApi.GetAll().Results;
+
+        using (new AssertionScope())
+        {
+            allProperties.Should().NotBeNull();
+            allProperties.Count.Should()
+                .BeGreaterOrEqualTo(2);
+            allProperties.Should().Contain(p => p.Name == createdCompanyProperty1.Name);
+            allProperties.Should().Contain(p => p.Name == createdCompanyProperty2.Name);
+        }
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldNotThrowException()
+    {
+        var act = async () => (await CompanyPropertiesApi.GetAllAsync()).Results;
+        await act.Should().NotThrowAsync<HubSpotException>();
+    }
+}
