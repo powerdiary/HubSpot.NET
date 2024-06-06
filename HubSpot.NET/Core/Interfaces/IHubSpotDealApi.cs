151e0819a0e02b3bcb416f6f32fe802bec5e1704
Oleg S <lom200@gmail.com>
2024-06-06 16:16:14 +1000

    Add async methods to HubSpotDealApi and implement integration tests
    This commit adds async implementations to the HubSpotDealApi. It covers methods like CreateAsync, GetByIdAsync, UpdateAsync, DeleteAsync, ListAsync, and SearchAsync. In addition, it includes a new set of integration tests for the Deal API to validate the new async methods and ensure they behave as expected.

--- HubSpot.NET/Core/Interfaces/IHubSpotDealApi.cs
+++ HubSpot.NET/Core/Interfaces/IHubSpotDealApi.cs
@@ -1,4 +1,5 @@
-﻿using HubSpot.NET.Api;
+﻿using System.Threading.Tasks;
+using HubSpot.NET.Api;
 using HubSpot.NET.Api.Deal;
 using HubSpot.NET.Api.Deal.Dto;
 
@@ -7,11 +8,19 @@ namespace HubSpot.NET.Core.Interfaces
     public interface IHubSpotDealApi
     {
         T Create<T>(T entity) where T : DealHubSpotModel, new();
+        Task<T> CreateAsync<T>(T entity) where T : DealHubSpotModel, new();
+
         void Delete(long dealId);
+        Task DeleteAsync(long dealId);
+
         T GetById<T>(long dealId) where T : DealHubSpotModel, new();
+        Task<T> GetByIdAsync<T>(long dealId) where T : DealHubSpotModel, new();
+
         T Update<T>(T entity) where T : DealHubSpotModel, new();
+        Task<T> UpdateAsync<T>(T entity) where T : DealHubSpotModel, new();
 
         DealListHubSpotModel<T> List<T>(DealListRequestOptions opts = null) where T : DealHubSpotModel, new();
+        Task<DealListHubSpotModel<T>> ListAsync<T>(DealListRequestOptions opts = null) where T : DealHubSpotModel, new();
 
         DealRecentListHubSpotModel<T> RecentlyCreated<T>(DealRecentRequestOptions opts = null)
             where T : DealHubSpotModel, new();
@@ -21,6 +30,8 @@ namespace HubSpot.NET.Core.Interfaces
         DealListHubSpotModel<T> ListAssociated<T>(bool includeAssociations, long hubId, ListRequestOptions opts = null, string objectName = "contact") where T :DealHubSpotModel, new();
 
         SearchHubSpotModel<T> Search<T>(SearchRequestOptions opts = null)
+            where T : DealHubSpotModel, new();
+        Task<SearchHubSpotModel<T>> SearchAsync<T>(SearchRequestOptions opts = null)
             where T : DealHubSpotModel, new();
 
         T AssociateToCompany<T>(T entity, long companyId)
