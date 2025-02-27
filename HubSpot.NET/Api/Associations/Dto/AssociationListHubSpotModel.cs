98c333fc4a210a7c7a1d62f1f7f8b3b6a5fcc962
Batjargal Bayarsaikhan <batjargal.bayarsaikhan@powerdiary.com>
2025-02-27 11:09:19 +0800

    Add GetAssociationsAsync in HubSpotAssociationsApi

---
+++ HubSpot.NET/Api/Associations/Dto/AssociationListHubSpotModel.cs
@@ -0,0 +1,40 @@
+﻿using HubSpot.NET.Core.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace HubSpot.NET.Api.Associations.Dto
+{
+    /// <summary>
+    /// Models the associations of a HubSpot Object to other HubSpot Objects
+    /// </summary>
+    [DataContract]
+    public class AssociationListHubSpotModel : IHubSpotModel
+    {
+        [JsonProperty(PropertyName = "results")]
+        public List<Association> Associations { get; set; } = new List<Association>();
+
+        public class Association
+        {
+            [JsonProperty(PropertyName = "toObjectId")]
+            public long AssociatedObjectId { get; set; }
+            public List<AssociationType> AssociationTypes { get; set; } = new List<AssociationType>();
+        }
+
+        public class AssociationType
+        {
+            public string Category { get; set; } // HUBSPOT_DEFINED or USER_DEFINED
+            public long TypeId { get; set; }
+            public string Label { get; set; } // e.g. "Contact with Primary Company"
+        }
+
+
+        public bool IsNameValue => false;
+
+        public void ToHubSpotDataEntity(ref dynamic dataEntity) { }
+
+        public void FromHubSpotDataEntity(dynamic hubspotData) { }
+
+        public string RouteBasePath => "/crm/v4/objects/";
+    }
+}
